0abef250eddecc418f87ccda7a3794481e6a29d0
hritik4651 <hritik4651@gmail.com>
2025-01-31 06:37:35 +0530

    add files

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get("/", (res, req) => {
+  req.send("Nodejs Project v2");
+});
+app.get("/api", (res, req) => {
+  req.send("API from express server.");
+});
+
+app.get("/api/user", (res, req) => {
+  req.send("API from express server. and u r a User");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at  ${port}`);
+});
